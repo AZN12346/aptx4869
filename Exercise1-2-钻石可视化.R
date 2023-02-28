6eca4790d086f40b48ad12f1485f1a8a0ef09175
AZN12346 <2982213544.com>
2023-02-28 19:57:33 +0800

    paolule

--- Exercise1-2-钻石可视化.R
+++ Exercise1-2-钻石可视化.R
@@ -3,8 +3,6 @@ require(ggplot2)
 data()
 data(diamonds)
 head(diamonds)
-#柱状图
-hist(diamonds$carat,main="Carat Histogram",xlab="Carat")
 #散点图
 plot(price~carat,data=diamonds)
 plot(log(price)~log(carat),data=diamonds)
@@ -13,5 +11,7 @@ ggplot(diamonds,aes(x=carat,y=price))+geom_point()
 ggplot(diamonds,aes(x=carat,y=price))+geom_point(aes(color=color))
 ggplot(diamonds,aes(x=carat,y=price))+geom_point(aes(color=color))+facet_grid(cut~clarity)
 #从 ggthemes 提取华尔街日报的图表配色
+#suibianlaidianba
 library(ggthemes)
 ggplot(diamonds,aes(x=carat,y=price))+geom_point(aes(color=color))+theme_wsj()
+
